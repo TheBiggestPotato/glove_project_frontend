d94f3b3b4fd023b06a0cee6b8bd308e31b90fbf7
Andrei <andrei.fabian282@gmail.com>
2022-11-26 00:28:05 +0200

    tab text component

---
+++ src/components/molecules/TabText/tabText.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./tabText.scss";
+import BigHeading from "../../atoms/BigHeading/bigHeading";
+import Line from "../../atoms/Line/line";
+import RoundButton from "../../atoms/RoundButton/roundButton";
+import SmallHeading from "../../atoms/SmallHeading/smallHeading";
+
+const TabText = () => {
+  return (
+    <div className="tabText">
+      <BigHeading color="black" content="Protejam viata" />
+      <BigHeading color="black" content="Protejam viitorul" />
+      <Line width="27%" height="2px"></Line>
+      <SmallHeading
+        color="black"
+        content="Producatorul european de manusi de nitril"
+      />
+      <RoundButton
+        className="tabText__btn"
+        buttonType="first"
+        label="Afla mai multe"
+      />
+    </div>
+  );
+};
+
+export default TabText;
--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,21 +1,11 @@
 import React from "react";
-import BigHeading from "../../atoms/BigHeading/bigHeading";
-import Line from "../../atoms/Line/line";
-import RoundButton from "../../atoms/RoundButton/roundButton";
-import SmallHeading from "../../atoms/SmallHeading/smallHeading";
+import TabText from "../../molecules/TabText/tabText";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
-      <BigHeading color="black" content="Protejam viata" />
-      <BigHeading color="black" content="Protejam viitorul" />
-      <Line width="27%" height="2px"></Line>
-      <SmallHeading
-        color="black"
-        content="Producatorul european de manusi de nitril"
-      />
-      <RoundButton buttonType="first" label="Afla mai multe" />
+      <TabText />
     </div>
   );
 };
