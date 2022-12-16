b4c217297d111fe7718b65628aa04883d3756273
catalingdiaconu <kata7299@gmail.com>
2022-12-16 11:52:15 +0200

    added Nav to Home and Pointer to button

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,12 +1,18 @@
 import React from "react";
+
 import TabText from "../../molecules/TabText/tabText";
+import Nav from "../../organisms/Nav/nav";
+
 import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className="home">
+    <>
+    <Nav />
+    <div className="home__body">
       <TabText />
     </div>
+    </>
   );
 };
 
