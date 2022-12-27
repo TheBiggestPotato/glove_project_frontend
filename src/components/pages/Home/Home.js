d199d62ca32b95a379b1ae991a07267faa1a0e44
catalingdiaconu <kata7299@gmail.com>
2022-12-27 13:53:05 +0200

    added .env for local IP

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,14 +1,12 @@
 import React from "react";
 
 import TabText from "../../molecules/TabText/tabText";
-import Nav from "../../organisms/Nav/nav";
 
 import "./Home.scss";
 
 const Home = () => {
   return (
     <>
-    <Nav />
     <div className="home__body">
       <TabText />
     </div>
